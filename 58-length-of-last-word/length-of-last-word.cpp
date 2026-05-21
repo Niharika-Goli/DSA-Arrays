9722da5eaa7c919d27e15ff7f3836a726d8b0a65
Niharika-Goli <niharikagoli14@gmail.com>
2026-05-21 21:39:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (5.24%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        istringstream iss(s);
+        string word;
+        vector<string>words;
+        while(iss >> word){
+            words.push_back(word);
+        }
+        string a=words.back();
+        return a.size();
+    }
+};
