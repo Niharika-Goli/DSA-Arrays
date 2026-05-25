c4ff540293f3f87595c386383d38827834bc8642
Niharika-Goli <niharikagoli14@gmail.com>
2026-05-25 15:26:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (16.42%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int l=0,h=num;
+        while (l<=h){
+            long long mid=(l+h)/2;
+            if ((mid*mid)==num){
+                return true;
+            }
+            else if ((mid*mid)<num){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return false;
+    }
+};
