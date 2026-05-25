9cd98a5a25eb8c83e9ce14b9baa189bed2c92136
Niharika-Goli <niharikagoli14@gmail.com>
2026-05-25 16:06:21 +0530

    Time: 2 ms (57.61%) | Memory: 7.8 MB (71.14%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -5,18 +5,16 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int l=1,r=n;
-        int ans=n;
         while (l<=r){
             int mid=l+(r-l)/2;
             if (isBadVersion(mid)){
-                ans=min(ans,mid);
                 r=mid-1;
             }
             else{
                 l=mid+1;
             }
         }
-        return ans;
+        return l;
         
     }
 };
