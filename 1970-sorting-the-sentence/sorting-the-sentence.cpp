9dec6a005f1fbaacdc08026014a59653012b7eae
Niharika-Goli <niharikagoli14@gmail.com>
2026-07-28 11:29:00 +0530

    Time: 3 ms (41.09%) | Memory: 8.3 MB (97.44%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        map<int,string>mpp;
+        string temp;
+        for (int i=0;i<s.size();i++){
+            if (isdigit(s[i])){
+                mpp[s[i]-'0']=temp;
+                temp="";
+                i++;
+            }
+            else{
+                temp+=s[i];
+            }
+        }
+        string ans;
+        for (auto &it:mpp){
+            ans+=it.second+" ";
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
