9a3fc108b322301501848003c4020a9671502309
Niharika-Goli <niharikagoli14@gmail.com>
2026-09-21 14:29:36 +0530

    Time: 1654 ms (46.28%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -2,4 +2,4 @@
 select s.name 
 from SalesPerson as s left join (orders as o join company as c on c.com_id=o.com_id and c.name="RED")
 on s.sales_id=o.sales_id
-where c.com_id IS NULL;
+where o.com_id IS NULL;
