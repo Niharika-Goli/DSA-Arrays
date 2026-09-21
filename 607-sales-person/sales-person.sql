8f0675aba80ad1b3f93ecbacde68d35250ca34f1
Niharika-Goli <niharikagoli14@gmail.com>
2026-09-21 14:28:28 +0530

    Time: 1666 ms (44.66%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,16 +1,5 @@
 # Write your MySQL query statement below
-#select s.name
-#from SalesPerson as s left join Orders as o on s.sales_id=o.sales_id
-#left join Company as c on c.com_id=o.com_id and c.name = "RED"
-#where c.com_id is NULL ;
-
-SELECT s.name
-FROM SalesPerson AS s
-LEFT JOIN (
-    Orders AS o
-    JOIN Company AS c
-    ON o.com_id = c.com_id
-    AND c.name = 'RED'
-)
-ON s.sales_id = o.sales_id
-WHERE o.sales_id IS NULL;
+select s.name 
+from SalesPerson as s left join (orders as o join company as c on c.com_id=o.com_id and c.name="RED")
+on s.sales_id=o.sales_id
+where c.com_id IS NULL;
