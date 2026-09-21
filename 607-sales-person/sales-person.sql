37177c64632884c25e01c5e60875def6ea380bbe
Niharika-Goli <niharikagoli14@gmail.com>
2026-09-21 15:10:03 +0530

    Time: 1466 ms (77.83%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,5 +1,15 @@
 # Write your MySQL query statement below
-select s.name 
-from SalesPerson as s left join (orders as o join company as c on c.com_id=o.com_id and c.name="RED")
-on s.sales_id=o.sales_id
-where o.com_id IS NULL;
+#select s.name 
+#from SalesPerson as s left join (orders as o join company as c on c.com_id=o.com_id)
+#on s.sales_id=o.sales_id
+#where o.com_id IS NULL;
+
+SELECT s.name
+FROM SalesPerson AS s
+LEFT JOIN Orders AS o
+    ON s.sales_id = o.sales_id
+LEFT JOIN Company AS c
+    ON c.com_id = o.com_id
+GROUP BY s.name, s.sales_id
+HAVING SUM(c.name = 'RED') = 0
+    OR SUM(c.name = 'RED') IS NULL;
